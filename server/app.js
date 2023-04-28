3945feb7b931d51da0931b2b854765faa0f2a868
Roman Kornev <kornev.roma62@gmail.com>
2023-04-28 14:02:59 +0300

    feat: add back Docker

--- server/app.js
+++ server/app.js
@@ -1,39 +1,37 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const path = require('path')
-const routes = require('./routes/note.routes')
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const routes = require("./routes/note.routes");
 
-const app = express()
-const port = process.env.PORT ?? 5000
+const app = express();
+const port = process.env.PORT ?? 5000;
 
-app.use(express.json({ extended: true }))
-app.use('/api/note', routes)
+app.use(express.json({ extended: true }));
+app.use("/api/note", routes);
 
-if (process.env.NODE_ENV === 'production') {
-  app.use('/', express.static(path.join(__dirname, '..', 'client', 'build')))
+if (process.env.NODE_ENV === "production") {
+  app.use("/", express.static(path.join(__dirname, "..", "client", "build")));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'))
-  })
+  app.get("*", (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, "..", "client", "build", "index.html")
+    );
+  });
 }
 
 async function start() {
   try {
-    await mongoose.connect(`mongodb://localhost:27017/notes`, {
+    await mongoose.connect(`mongodb://host.docker.internal:27017/notes`, {
       useNewUrlParser: true,
-      useUnifiedTopology: true
-    })
+      useUnifiedTopology: true,
+    });
     app.listen(
       port,
       console.log.bind(console, `Server has been started on port ${port}`)
-    )
+    );
   } catch (e) {
-    console.log(e)
+    console.log(e);
   }
 }
 
-start()
-
-
-
-
+start();
